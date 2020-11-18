322c4ac0016d56bb1254f9e1ca4269dae846f786
shanleycorvite <51499722+shanleycorvite@users.noreply.github.com>
2020-11-18 10:10:51 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -6,8 +6,15 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play()
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	console.log("Pause Video");
+	video.pause()
 });
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *=.9;
+	console.log("The speed is "+video.playbackRate);
+})
